15f5bc8440230c11c649894e4c6464515f5ad62e
Edvin Mohlin <edvin.mohlin@gmail.com>
2023-10-19 18:39:15 +0200

    Fixed touch ISRs

--- src/Touch.cpp
+++ src/Touch.cpp
@@ -3,9 +3,16 @@
 
 #define POLL_DELAY_MS 150
 #define TOUCH_WAIT_MS 200
+#define TOUCH_THRESHOLD 12
 
 
-bool newTouch(TickType_t* lastTouch)
+struct isrArgs{
+    uint8_t pin;
+    touch_value_t threshold;
+    QueueHandle_t queue;   
+};
+
+bool IRAM_ATTR newTouch(TickType_t* lastTouch)
 {
     TickType_t tick = xTaskGetTickCountFromISR();
     bool newTouch = pdTICKS_TO_MS(tick - *lastTouch) > TOUCH_WAIT_MS;
@@ -13,27 +20,27 @@ bool newTouch(TickType_t* lastTouch)
     return newTouch;
 }
 
-void IRAM_ATTR powerIsr(void* queue)
+void IRAM_ATTR handleIsr(struct isrArgs* args, touch type, TickType_t* lastTick)
 {
-    static TickType_t lastTick = 0;
-    if(touchRead(12) > 10)
+    if(touchRead(args->pin) > args->threshold)
         return;
-    touch val = touch::power;
     BaseType_t woken = false;
-    if(newTouch(&lastTick))
-        xQueueSendFromISR((QueueHandle_t)queue, &val, &woken);
-    
+    if(newTouch(lastTick))
+        xQueueSendFromISR(args->queue, &type, &woken);
 }
-void IRAM_ATTR modeIsr(void* queue)
+
+void IRAM_ATTR powerIsr(void* args)
 {
     static TickType_t lastTick = 0;
 
-    if(touchRead(32) > 10)
-        return;
-    touch val = touch::mode;
-    BaseType_t woken = false;
-    if(newTouch(&lastTick))
-        xQueueSendFromISR((QueueHandle_t)queue, &val, &woken);
+    handleIsr((struct isrArgs*)args, touch::power, &lastTick);
+}
+
+void IRAM_ATTR modeIsr(void* args)
+{
+    static TickType_t lastTick = 0;
+
+    handleIsr((struct isrArgs*)args, touch::mode, &lastTick);
 }
 
 
@@ -62,17 +69,26 @@ void touchTask(void* touchArgs)
 
 BaseType_t initTouch(Controller* controller, uint8_t powerPin, uint8_t modePin)
 {
-    struct touchTaskArgs* args = (struct touchTaskArgs*)malloc(sizeof(struct touchTaskArgs));
-    QueueHandle_t q = xQueueCreate(5, sizeof(touch));
-    if(args == NULL || q == NULL)
+    struct touchTaskArgs* taskArgs = (struct touchTaskArgs*)malloc(sizeof(struct touchTaskArgs));
+    struct isrArgs* pIsrArgs = (struct isrArgs*)malloc(2*sizeof(struct isrArgs));
+    if(taskArgs == NULL || pIsrArgs == NULL)
         return errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY;
     
-    *args = {q, controller};
-    if(xTaskCreate(touchTask, "touch", configMINIMAL_STACK_SIZE + 1024, args, 1, NULL) != pdPASS)
+    struct isrArgs* mIsrArgs = pIsrArgs+1;
+    
+
+    QueueHandle_t q = xQueueCreate(5, sizeof(touch));
+    if(taskArgs == NULL || q == NULL)
+        return errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY;
+    
+    *pIsrArgs = {powerPin, TOUCH_THRESHOLD, q};
+    *mIsrArgs = {modePin, TOUCH_THRESHOLD, q};
+    *taskArgs = {q, controller};
+    if(xTaskCreate(touchTask, "touch", configMINIMAL_STACK_SIZE + 1024, taskArgs, 1, NULL) != pdPASS)
         return errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY;
 
-    touchAttachInterruptArg(powerPin, powerIsr, q, 10);
-    touchAttachInterruptArg(modePin, modeIsr, q, 10);
+    touchAttachInterruptArg(powerPin, powerIsr, pIsrArgs, pIsrArgs->threshold);
+    touchAttachInterruptArg(modePin, modeIsr, mIsrArgs, mIsrArgs->threshold);
     return pdPASS;
 }
 
