9621c940d7007842091581fca0ab925334a70a9c
Edvin Mohlin <edvin.mohlin@gmail.com>
2024-01-16 19:01:24 +0100

    Fixed bug

--- include/MqttPayloads.h
+++ include/MqttPayloads.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "MqttTopics.h"
+#include "Modes.h"
 
 // Number to string
 #define NTS(x) STR(x)
@@ -14,7 +15,7 @@
 #define POWER_ON_PAYLOAD "ON"
 #define POWER_OFF_PAYLOAD "OFF"
 
-#define HA_DISCOVERY_SPEED_PAYLOAD "{\"name\": \"Speed\", \"state_topic\": \"" SPEED_STATE_TOPIC "\", \"command_topic\": \"" SPEED_SET_TOPIC "\", \"device_class\": \"power_factor\", \"availability\": [{\"topic\": \"" AVAILABILITY_TOPIC "\"}, {\"topic\": \"" POWER_STATE_TOPIC "\", \"payload_available\": \"" POWER_ON_PAYLOAD "\", \"payload_not_available\": \"" POWER_OFF_PAYLOAD "\"}], \"unique_id\": \"hum1speed\", \"min\": " NTS(PWM_LOW) ", \"max\": " NTS(PWM_MAX) ", \"device\": " HA_DEVICE "}"
+#define HA_DISCOVERY_SPEED_PAYLOAD "{\"name\": \"Speed\", \"state_topic\": \"" SPEED_STATE_TOPIC "\", \"command_topic\": \"" SPEED_SET_TOPIC "\", \"device_class\": \"power_factor\", \"availability\": [{\"topic\": \"" AVAILABILITY_TOPIC "\"}, {\"topic\": \"" POWER_STATE_TOPIC "\", \"payload_available\": \"" POWER_ON_PAYLOAD "\", \"payload_not_available\": \"" POWER_OFF_PAYLOAD "\"}], \"unique_id\": \"hum1speed\", \"min\": " NTS(PWM_LOW)", \"max\": " NTS(PWM_MAX)", \"device\": " HA_DEVICE "}"
 
 #define HA_DISCOVERY_WATER_PAYLOAD "{\"name\": \"Water\", \"device_class\": \"moisture\", \"state_topic\": \"" WATER_STATE_TOPIC "\", \"availability_topic\": \"" AVAILABILITY_TOPIC "\", \"unique_id\": \"hum1water\", \"device\": " HA_DEVICE "}"
 #define WATER_GOOD_PAYLOAD "ON"
